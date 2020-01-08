c3da386cc3f534a56a53f428e6324102b167d51a
easypeng <344553788@qq.com>
2020-01-08 23:10:23 +0800

    add pageSize

--- src/main/java/cn/demo/protocal/request/RedisDBRequest.java
+++ src/main/java/cn/demo/protocal/request/RedisDBRequest.java
@@ -2,6 +2,18 @@ package cn.demo.protocal.request;
 
 import java.io.Serializable;
 
+import lombok.Data;
+
+@Data
 public class RedisDBRequest implements Serializable {
 
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4616859026616266360L;
+
+	private Integer pageNum;
+	
+	private Integer pageSize;
 }
